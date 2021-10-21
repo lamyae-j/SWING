497f8de6675922605432c52b1115ec2adb1fce98
jdid <lamyaejdid20@gmail.com>
2021-10-21 23:21:20 +0100

    ajout du boutton

--- Cercle.java
+++ Cercle.java
@@ -3,15 +3,20 @@ package Dessin;
 import java.awt.Graphics;
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 //pour afficher une balle sur l'ecran
+// first qs
 
 public class Cercle extends JPanel
 {
 
     public static int N =220;
     public static int M=300;
+
+    // generation de balles pleines
     public void paint(Graphics g){
         int x=5,y=4,z=15,h=15;
         do{
@@ -24,18 +29,35 @@ public class Cercle extends JPanel
                 y+=20;
                 x=5;
             }
-
         }while(y<N);
-
-
     }
 
     public static void main(String[] args){
         JFrame f = new JFrame("Dessiner un cercle");
+
+        //Créer le bouton
+        JButton btn = new JButton("Cliquez ici");
+        Cercle cercle = new Cercle();
+        //Définir la position du bouton
+        btn.setBounds(50,200,100,40);
+        // presser le bouton
+        btn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+
+        f.add(btn);
         f.getContentPane().add(new Cercle());
+        f.setSize(300,300);
+        f.setLayout(null);
+
+
         f.setSize(N, M);
         f.setVisible(true);
         f.setResizable(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
